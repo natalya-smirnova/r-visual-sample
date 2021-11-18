4127429b75a3ec18f75b0d8e7ccec78e5396a1cd
natalya-smirnova <v-nsmirnova@microsoft.com>
2021-11-18 14:33:44 +0300

    Add r-script

--- script.r
+++ script.r
@@ -1,1 +1,8 @@
-plot(Values);
+plot(dataset)
+x <- dataset[,1] # get the first column from dataset
+y <- dataset[,2] # get the second column from dataset
+
+columnNames = colnames(dataset) # get column names
+
+plot(x, y, type="n", xlab=columnNames[1], ylab=columnNames[2]) # draw empty plot with axis and labels only
+lines(x, y, col="green") # draw line plot
