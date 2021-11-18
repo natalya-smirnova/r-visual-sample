9ef6dea5a05abc3cea1bd3b212405c9b30bbe8e3
natalya-smirnova <v-nsmirnova@microsoft.com>
2021-11-18 15:11:46 +0300

    Add corrplot

--- script.r
+++ script.r
@@ -6,3 +6,7 @@ columnNames = colnames(dataset) # get column names
 
 plot(x, y, type="n", xlab=columnNames[1], ylab=columnNames[2]) # draw empty plot with axis and labels only
 lines(x, y, col="green") # draw line plot
+
+library(corrplot)
+corr <- cor(dataset)
+corrplot(corr, method="circle", order = "hclust")
